52415451ea466c0dde2c2d74204445281669f4cc
Marouche <116162435+Marouchee@users.noreply.github.com>
2023-05-15 14:10:36 +0900

    5/13 boj_11403 sol

---
+++ Floyd-Warshall/boj_11403.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+
+graph=[]
+
+for _ in range(n):
+  graph.append(list(map(int, input().split())))
+
+for k in range(n):
+  for i in range(n):
+    for j in range(n):
+      if graph[i][k]+graph[k][j]>1:
+       graph[i][j]=1
+
+for i in range(n):
+  for j in range(n):
+    print(graph[i][j], end=' ')
+  print()
