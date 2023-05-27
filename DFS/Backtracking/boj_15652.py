2d3da70ba4315b10e50ea1d46f9d5cb01a79d049
Marouche <116162435+Marouchee@users.noreply.github.com>
2023-05-27 15:36:27 +0900

    5/27 boj_15652 sol

---
+++ DFS/Backtracking/boj_15652.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n,m=map(int, input().split())
+
+s=[]
+
+def dfs():
+  if len(s)==m:
+    print(' '.join(map(str,s)))
+    return
+    
+  for i in range(1, n+1):
+    if len(s)==0:
+      s.append(i)
+      dfs()
+      s.pop()
+    elif i>=s[-1]:
+      s.append(i)
+      dfs()
+      s.pop()
+
+dfs()
