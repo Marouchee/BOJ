a3e93a1d071efe9ad9e78b02edf7ac2c5a0538d4
Marouche <116162435+Marouchee@users.noreply.github.com>
2023-06-09 15:49:36 +0900

    6/9 boj_1916 sol

---
+++ Dijkstra/boj_1916.py
@@ -0,0 +1,39 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+
+graph=[[] for _ in range(n+1)]
+distance= [INF] * (n+1)
+
+m = int(input())
+
+for _ in range(m):
+  a,b,c=map(int, input().split())
+  graph[a].append((b,c))
+
+def dijkstra(start):
+  q=[]
+
+  heapq.heappush(q, (0, start))
+  distance[start]=0
+
+  while q:
+    dist, now = heapq.heappop(q)
+
+    if distance[now] < dist:
+      continue
+
+    for i in graph[now]:
+      cost = dist +i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]]=cost
+        heapq.heappush(q, (cost, i[0]))
+
+start, end = map(int, input().split())
+
+dijkstra(start)
+
+print(distance[end])
