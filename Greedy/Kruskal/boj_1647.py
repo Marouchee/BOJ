851abf99779a7fb06022a83ffca59358ead864f0
Marouche <116162435+Marouchee@users.noreply.github.com>
2023-07-01 16:38:45 +0900

    6/30 boj_1647 sol

---
+++ Greedy/Kruskal/boj_1647.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+n, m = map(int, input().split())
+parent = [0]*(n+1)
+
+for i in range(1, n+1):
+    parent[i] = i
+
+edges = []
+result = 0
+now = 0
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        now = cost
+        result += now
+
+print(result - now)
