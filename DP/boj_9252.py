b035a24cb30cba2f544ad713821eeed72f3d28d5
Marouche <116162435+Marouchee@users.noreply.github.com>
2023-07-09 12:01:06 +0900

    7/9 boj_9252 sol

---
+++ DP/boj_9252.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+s1 = input().rstrip()
+s2 = input().rstrip()
+
+dp = [[0]*(len(s1)+1) for _ in range(len(s2)+1)]
+dp2 = [['']*(len(s1)+1) for _ in range(len(s2)+1)]
+
+for i in range(1, len(s2)+1):
+    for j in range(1, len(s1)+1):
+        if s2[i-1] == s1[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+            dp2[i][j] = dp2[i-1][j-1] + s1[j-1]
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            if dp[i][j-1] > dp[i-1][j]:
+                dp2[i][j] = dp2[i][j-1]
+            else:
+                dp2[i][j] = dp2[i-1][j]
+
+print(dp[-1][-1])
+if dp[-1][-1] != 0:
+    print(dp2[-1][-1])
